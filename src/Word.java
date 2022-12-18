b07a1d655f3cb5bfd274eae9a3bb44acaa0c8e6f
kkettch <maria.chmurova884@gmail.com>
2022-12-18 17:35:08 +0300

    new commit

---
+++ src/Word.java
@@ -0,0 +1,9 @@
+public class Word {
+    protected String name;
+    protected boolean difficult;
+
+    Word(String name, boolean difficult) {
+        this.name = name;
+        this.difficult = difficult;
+    }
+}
